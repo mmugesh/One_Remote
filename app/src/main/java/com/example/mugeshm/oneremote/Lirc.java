3b9e65ad321f3be3e21622c9060c3625de87a8e8
Mugesh <mmugesh95@gmail.com>
2016-02-02 15:39:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mugeshm/oneremote/Lirc.java
@@ -0,0 +1,24 @@
+package com.example.mugeshm.oneremote;
+
+import java.io.File;
+
+/**
+ * Created by Mugesh M on 27-01-2016.
+ */
+public class Lirc {
+   // public static String POWER_TOGGLE = "Function18";
+
+    static {
+        System.loadLibrary("androlirc");
+    }
+    native int parse(String filename);
+    native byte[] getIrBuffer(String irDevice, String irCode, int minBufSize);
+    native String[] getDeviceList();
+    native String[] getCommandList(String irDevice);
+
+    Lirc (){
+
+        File dir = new File("/data/data/com.example.mugeshm.oneremote/log");
+        dir.mkdirs();
+    }
+}
