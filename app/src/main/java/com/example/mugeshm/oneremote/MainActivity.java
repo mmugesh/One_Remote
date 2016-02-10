4a39d8968f466b384897a6a15834a2deee9e6414
Mugesh <mmugesh95@gmail.com>
2016-02-10 13:25:55 +0530

    file browser

--- app/src/main/java/com/example/mugeshm/oneremote/MainActivity.java
+++ app/src/main/java/com/example/mugeshm/oneremote/MainActivity.java
@@ -1,23 +1,24 @@
 package com.example.mugeshm.oneremote;
 
 import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -25,8 +26,11 @@ import android.widget.Toast;
 
 import com.zokama.androlirc.Lirc;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
    private final static String LIRCD_CONF_FILE = "/data/data/com.example.mugeshm.oneremote/lircd.conf";
@@ -39,9 +43,22 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> commandList;
     AudioTrack ir;
     int minBufSize;
-    boolean playLock;
+    String address;
+    ArrayList<String> str = new ArrayList<String>();
 
+    // Check if the first level of the directory structure is the one showing
+    private Boolean firstLvl = true;
 
+    private static final String TAG = "F_PATH";
+
+    private Item[] fileList;
+    private File path = new File(Environment.getExternalStorageDirectory() + "");
+    private String chosenFile;
+    private static final int DIALOG_LOAD_FILE = 1000;
+
+    ListAdapter adapter;
+
+    private static final int PICKFILE_RESULT_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -121,37 +138,177 @@ public class MainActivity extends AppCompatActivity {
 
         ir.play();
     }
-    public String selectFile(){
+    public void loadFileList() {
+        try {
+            path.mkdirs();
+        } catch (SecurityException e) {
+            Log.e(TAG, "unable to write on the sd card ");
+        }
 
-        final EditText ed = new EditText(this);
+        // Checks whether path exists
+        if (path.exists()) {
+            FilenameFilter filter = new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String filename) {
+                    File sel = new File(dir, filename);
+                    // Filters based on whether the file is hidden or not
+                    return (sel.isFile() || sel.isDirectory())
+                            && !sel.isHidden();
 
-        Builder builder = new Builder(this);
-        builder.setTitle("Select a file to parse");
-        builder.setView(ed);
-        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if (which == Dialog.BUTTON_NEGATIVE) {
-                    dialog.dismiss();
-                    return;
                 }
+            };
 
-                parse(ed.getText().toString());
-            }});
+            String[] fList = path.list(filter);
+            fileList = new Item[fList.length];
+            for (int i = 0; i < fList.length; i++) {
+                fileList[i] = new Item(fList[i], R.drawable.file_icon);
 
-        builder.setNegativeButton("Cancel", null);
-        final AlertDialog asDialog = builder.create();
-        asDialog.show();
+                // Convert into file path
+                File sel = new File(path, fList[i]);
+
+                // Set drawables
+                if (sel.isDirectory()) {
+                    fileList[i].icon = R.drawable.directory_icon;
+                    Log.d("DIRECTORY", fileList[i].file);
+                } else {
+                    Log.d("FILE", fileList[i].file);
+                }
+            }
+
+            if (!firstLvl) {
+                Item temp[] = new Item[fileList.length + 1];
+                for (int i = 0; i < fileList.length; i++) {
+                    temp[i + 1] = fileList[i];
+                }
+                temp[0] = new Item("Up", R.drawable.directory_up);
+                fileList = temp;
+            }
+        } else {
+            Log.e(TAG, "path does not exist");
+        }
+
+        adapter = new ArrayAdapter<Item>(this,
+                android.R.layout.select_dialog_item, android.R.id.text1,
+                fileList) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                // creates view
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view
+                        .findViewById(android.R.id.text1);
+
+                // put the image on the text view
+                textView.setCompoundDrawablesWithIntrinsicBounds(
+                        fileList[position].icon, 0, 0, 0);
+
+                // add margin between image and text (support various screen
+                // densities)
+                int dp5 = (int) (5 * getResources().getDisplayMetrics().density + 0.5f);
+                textView.setCompoundDrawablePadding(dp5);
+
+                return view;
+            }
+        };
+
+    }
+
+    private class Item {
+        public String file;
+        public int icon;
+
+        public Item(String file, Integer icon) {
+            this.file = file;
+            this.icon = icon;
+        }
+
+        @Override
+        public String toString() {
+            return file;
+        }
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        if (fileList == null) {
+            Log.e(TAG, "No files loaded");
+            dialog = builder.create();
+            return dialog;
+        }
+
+        switch (id) {
+            case DIALOG_LOAD_FILE:
+                builder.setTitle("Select a file to parse");
+                builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        chosenFile = fileList[which].file;
+                        File sel = new File(path + "/" + chosenFile);
+                        if (sel.isDirectory()) {
+                            firstLvl = false;
+
+                            // Adds chosen directory to list
+                            str.add(chosenFile);
+                            fileList = null;
+                            path = new File(sel + "");
+
+                            loadFileList();
+
+                            removeDialog(DIALOG_LOAD_FILE);
+                            showDialog(DIALOG_LOAD_FILE);
+                            Log.d(TAG, path.getAbsolutePath());
+
+                        }
+
+                        // Checks if 'up' was clicked
+                        else if (chosenFile.equalsIgnoreCase("up") && !sel.exists()) {
+
+                            // present directory removed from list
+                            String s = str.remove(str.size() - 1);
+
+                            // path modified to exclude present directory
+                            path = new File(path.toString().substring(0,
+                                    path.toString().lastIndexOf(s)));
+                            fileList = null;
+
+                            // if there are no more directories in the list, then
+                            // its the first level
+                            if (str.isEmpty()) {
+                                firstLvl = true;
+                            }
+                            loadFileList();
+
+                            removeDialog(DIALOG_LOAD_FILE);
+                            showDialog(DIALOG_LOAD_FILE);
+                            Log.d(TAG, path.getAbsolutePath());
+
+                        }
+                        // File picked
+                        else {
+                            // Perform action with file picked
+                            address  = sel.getAbsolutePath();
+
+                            parse(address);
+                        }
+
+                    }
+                });
+                break;
+        }
+        dialog = builder.show();
+        return dialog;
+    }
+    public String selectFile() {
+
+        loadFileList();
+        showDialog(DIALOG_LOAD_FILE);
+        Log.d(TAG, path.getAbsolutePath());
         return null;
     }
 
-
-
-
     public boolean parse (String config_file) {
-
-        //Toast.makeText(getApplicationContext(), "Parsing "+config_file, Toast.LENGTH_SHORT).show();
 
         java.io.File file = new java.io.File(config_file);
         if (!file.exists()) {
@@ -232,13 +389,8 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "No command found for the selected device", Toast.LENGTH_SHORT).show();
             return;
         }
-
-
-
-
-
-
         commandList.clear();
+
         for (int i=0; i<str.length; i++)
             commandList.add(str[i]);
 
